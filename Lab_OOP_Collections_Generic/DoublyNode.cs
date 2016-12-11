b3761ad39be5c11350918f1ed9a1acc20271dee3
K-O-G <KaterinaO.Gordiyenko@livenau.net>
2016-12-12 01:38:50 +0200

    deque

---
+++ Lab_OOP_Collections_Generic/DoublyNode.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab_OOP_Collections_Generic
+{
+    public class DoublyNode<T>
+    {
+        public DoublyNode(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+    }
+}
