6a1a4c4e439c2e9e55997d95ade8ed21b82e676c
K-O-G <KaterinaO.Gordiyenko@livenau.net>
2016-12-11 23:59:32 +0200

    Добавьте файлы проекта.

---
+++ Lab_OOP_Collections_Generic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_OOP_Collections_Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
