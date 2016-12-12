6b71c1df15124bfdcbba3e8ba9f3f32fbd5a7520
K-O-G <KaterinaO.Gordiyenko@livenau.net>
2016-12-12 09:01:26 +0200

    1

--- Lab_OOP_Collections_Generic/Program.cs
+++ Lab_OOP_Collections_Generic/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static Lab_OOP_Collections_Generic.UI;
 
 namespace Lab_OOP_Collections_Generic
